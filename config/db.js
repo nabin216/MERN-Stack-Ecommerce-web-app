e5bb9a87cc4224211d51fa24aae06328ea4266ee
nabin216 <110443206+nabin216@users.noreply.github.com>
2024-04-08 17:03:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect("mongodb+srv://asadurzamannabin:nabin@cluster0.srlms0c.mongodb.net/");
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
